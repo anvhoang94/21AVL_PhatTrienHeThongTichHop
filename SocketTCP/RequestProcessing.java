f3462620e851ce903c61a4de53ed2f7c47b6410a
Hoang Vu An <115540971+anvhoang94@users.noreply.github.com>
2026-09-14 20:01:04 +0700

    update

---
+++ SocketTCP/RequestProcessing.java
@@ -0,0 +1,26 @@
+package SocketTCP;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class RequestProcessing extends Thread {
+	Socket channel; //socket của kênh ảo với clien hiện tại
+	public RequestProcessing(Socket s) {
+		channel = s;
+	}
+	public void run() {
+		try {
+			OutputStream os = channel.getOutputStream();
+			InputStream is = channel.getInputStream();
+			while (true) {
+				int n = is.read();
+				if(n==-1) break;
+				os.write(n);
+			}
+		}catch(IOException ie) {
+			System.out.println("Request Processing Error: "+ie);
+		}
+	}
+}
